6f73b1a78442f165129ffcd01a786b9488c04ff7
Evan Anderson <epanderson92@yahoo.com>
2016-11-29 22:38:38 -0600

    twitter key applied

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<RydsPI8EhDgtwpJMhzznfvFao>',
+  consumer_secret: '<od8iUQX1nFKOWCBaHWiCWyagqHyzK9F2kClXR5a5Cwv2pthXHf>',
+  access_token_key: '<Access Token	803750974967975936-SGhds1X9KAM7WcOnzLJUqBZQuU1irrs>',
+  access_token_secret: '<D90nWBRHbFjcUC2GPvCGmcMMTprEBqZJ7jR1fH0SmKrp7>',
+}
