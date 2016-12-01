5d2f148a2b3dc9508846f9c53a83a860fc225d5e
Evan Anderson <epanderson92@yahoo.com>
2016-11-30 23:44:33 -0600

    reworking code to get tweets

--- liri.js
+++ liri.js
@@ -4,17 +4,33 @@ var Twitter = require("twitter");
 var arg = proccess.argv[2]
 if(arg === "my-tweets"){
 	tweets();
+
+
+	var param = {screen_name; "e_ander512"};
+client.get("status/user_timeline", param, function(error, tweets, respost){
+	if (!error){
+		for(var i = 0; < 4; i++){
+			console.log(tweets[i].text, tweets[i].created_at);
+		}
+	}
+	});	
+};
+
+
 }
-// ----------twitter----------- //
 
-// var client = new twitter()
 
-var client = new Twitter({
+/*var client = new Twitter({
   consumer_key: 'RydsPI8EhDgtwpJMhzznfvFao',
   consumer_secret: 'od8iUQX1nFKOWCBaHWiCWyagqHyzK9F2kClXR5a5Cwv2pthXHf',
   access_token_key: '803750974967975936-SGhds1X9KAM7WcOnzLJUqBZQuU1irrs',
   access_token_secret: 'D90nWBRHbFjcUC2GPvCGmcMMTprEBqZJ7jR1fH0SmKrp7'
-});
+	});*/
+
+
+
+
+
 
 
 /*client.get(path, params, callback);
@@ -24,12 +40,5 @@ client.stream(path, params, callback);*/
 
 
 
-var param = {screen_name; "e_ander512"};
-client.get("status/user_timeline", param, function(error, tweets, respost){
-	if (!error){
-		for(var i = 0; < 4; i++){
-			console.log(tweets[i].text, tweets[i].created_at);
-		}
-	}
-	});	
-};
+
+
