125342e072ec0e13bcd8e2db9e6d5f5bf3ba5f8f
Evan Anderson <epanderson92@yahoo.com>
2016-12-12 21:42:54 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-  consumer_key: 'RydsPI8EhDgtwpJMhzznfvFao',
-  consumer_secret: 'od8iUQX1nFKOWCBaHWiCWyagqHyzK9F2kClXR5a5Cwv2pthXHf',
-  access_token_key: '803750974967975936-SGhds1X9KAM7WcOnzLJUqBZQuU1irrs',
-  access_token_secret: 'D90nWBRHbFjcUC2GPvCGmcMMTprEBqZJ7jR1fH0SmKrp7',
-}
